6feb65d698436b78b417d979f76951af529855c5
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2023-10-29 17:14:02 +0330

    Add MainForm_Load event and set value of some property

--- DatabaseReader/MainForm.cs
+++ DatabaseReader/MainForm.cs
@@ -1,10 +1,14 @@
-namespace DatabaseReader
+namespace DatabaseReader;
+
+public partial class MainForm : Form
 {
-    public partial class MainForm : Form
+    public MainForm() => InitializeComponent();
+
+    private void MainForm_Load(object sender, EventArgs e)
     {
-        public MainForm()
-        {
-            InitializeComponent();
-        }
+        AuthenticationComboBox.DataSource = new string[] { "Windows Authentication", "SQL Server Authentication" };
+        DatabasesGroupBox.Enabled = false;
+        UserNameLabel.Enabled = PasswordLabel.Enabled = false;
+        UserNameTextBox.Enabled = PasswordTextBox.Enabled = false;
     }
 }
