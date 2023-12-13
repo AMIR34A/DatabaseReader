801fd3308664885001fc7652aba2f986a0332215
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2023-12-13 22:12:33 +0330

    Edit IRepository

--- DatabaseReader/Repositories/IRepository.cs
+++ DatabaseReader/Repositories/IRepository.cs
@@ -8,5 +8,5 @@ public interface IRepository : IDisposable
     //Task<SqlDataReader> ExecuteSQLCommandAsync(string query);
     Task<DataTable> ExecuteSQLCommandAsync(string query);
     void UpdateConnectionString(string connectionString);
-    Task<int> GetCount(string tabelName);
+    Task<int> GetCountAsync(string tableName);
 }
