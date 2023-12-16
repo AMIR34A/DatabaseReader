83f405917a50546245e84da7debc248bdad22400
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2023-12-16 23:49:24 +0330

    Edit the parameters of DeleteAsync method

--- DatabaseReader/Repositories/IRepository.cs
+++ DatabaseReader/Repositories/IRepository.cs
@@ -1,13 +1,11 @@
-﻿using Microsoft.Data.SqlClient;
-using System.Data;
+﻿using System.Data;
 
 namespace DatabaseReader.Repositories;
 
 public interface IRepository : IDisposable
 {
-    //Task<SqlDataReader> ExecuteSQLCommandAsync(string query);
     Task<DataTable> ExecuteSQLCommandAsync(string query);
     void UpdateConnectionString(string connectionString);
     Task<int> GetCountAsync(string tableName);
-    Task<bool> DeleteAsync(string tableName, string id);
+    Task<bool> DeleteAsync(string tableName,string deleteBy, string value);
 }
