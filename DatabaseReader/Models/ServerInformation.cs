27760d48fd7ce0a412f78ae3bff27cee4b2649b0
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2024-01-15 23:50:26 +0330

    Add AuthenticationMode property into ServerInformation model

--- DatabaseReader/Models/ServerInformation.cs
+++ DatabaseReader/Models/ServerInformation.cs
@@ -5,4 +5,5 @@ public class ServerInformation
     public string Server { get; set; }
     public string Username { get; set; }
     public string Password { get; set; }
+    public AuthenticationMode AuthenticationMode { get; set; }
 }
